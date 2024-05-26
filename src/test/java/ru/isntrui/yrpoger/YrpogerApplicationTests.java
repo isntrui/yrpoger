e89be1dfcaf5604c1aa3ce0bf996302a11f1c13f
Artem Akopian <akopartem1@bk.ru>
2024-05-26 17:59:40 +0300

    init

---
+++ src/test/java/ru/isntrui/yrpoger/YrpogerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isntrui.yrpoger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YrpogerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
